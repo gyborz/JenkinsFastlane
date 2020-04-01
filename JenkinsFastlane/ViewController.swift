71192ccaf0478bee8e660d7f6a3e0e33e9330d94
gyborz <borzgy95@gmail.com>
2020-04-01 18:38:17 +0200

    Initial Commit

---
+++ JenkinsFastlane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsFastlane
+//
+//  Created by Gyorgy Borz on 2020. 04. 01..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
